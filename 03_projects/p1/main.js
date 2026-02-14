be53ab784610e7c0ceef0e6b68bd66f79b94c836
KiShuKyu <krishnadhiman8527@gmail.com>
2026-02-14 15:34:46 +0530

    Completed One simple project

--- 03_projects/p1/main.js
+++ 03_projects/p1/main.js
@@ -1,11 +1,26 @@
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector("body");
 
+
+// events in js 
+// jb bhi event click krte h us evenmt ko function me le skte h aaur uske saath kuch activity kr skte h
+
 buttons.forEach(function (button){
     console.log(button);
-    button.addEventListener('click', function(event) {
-        console.log(event)
+    button.addEventListener('click', function(e) {
+        console.log(e)
         console.log(e.target);
-        
+        if(e.target.id === "grey"){
+            body.style.backgroundColor = e.target.id; // body = grey
+        }
+        if(e.target.id === "white"){
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id === "yellow"){
+            body.style.backgroundColor = e.target.id;
+        }
     })
 })
